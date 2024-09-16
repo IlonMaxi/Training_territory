17e38019d60a4db3b18ed2efa6c41e71fe6c4dca
IlonMaxi <108355205+IlonMaxi@users.noreply.github.com>
2024-09-16 23:34:52 +0300

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,13 +37,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    ['@nuxtjs/tailwindcss', '@nuxtjs/framer-motion']
-    
   ],
-
-  tailwindcss: {
-      
-  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
